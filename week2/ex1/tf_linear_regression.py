5b3afd3fc4ecfe7de32eaa9f144eb8952386d35d
gicheonkang <kgc519@gmail.com>
2017-11-13 17:40:58 +0900

    tensorflow linear reg.

--- week2/ex1/tf_linear_regression.py
+++ week2/ex1/tf_linear_regression.py
@@ -2,41 +2,36 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-# load & data settings
 data = np.loadtxt('ex1data1.txt', unpack=True, dtype=np.float32, delimiter=',')
 data = data.transpose()
+# data shape (97,2)
 
-# It is easy to calculate X, Y training data is vector
-Xdata = data[:, 0]
-Ydata = data[:, 1]
+# note : x*theta dim == y dim
+x = np.concatenate((np.ones((len(data), 1)), np.reshape(data[:, 0], (len(data), 1))), axis=1)
+print(x)
 
-# placeholder for numpy array to tensorflow array !
+y = np.reshape(data[:, 1], (len(data), 1))
+theta = tf.Variable(tf.random_uniform([2, 1], -1.0, 1.0))
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
-# theta(weight) and bias
-theta = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-bias = tf.Variable(0.0)
-hypothesis = X*theta + bias
-
+hypothesis = tf.matmul(X, theta)
 cost_function = tf.reduce_mean(tf.square(hypothesis-Y))
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost_function)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    for i in range(50000):
-        sess.run([optimizer, cost_function], feed_dict={X: Xdata, Y: Ydata})
+    for i in range(10000):
+        sess.run([optimizer, cost_function], feed_dict={X:x, Y:y})
         if i % 1000 == 0:
-            print(sess.run([cost_function, theta], feed_dict={X: Xdata, Y: Ydata}))
+            print(sess.run([cost_function, theta], feed_dict={X:x, Y:y}))
 
-    wb, bb = sess.run([theta, bias], feed_dict={X:Xdata, Y:Ydata})
-    print('theta :', wb)
-    print('bias : ', bb)
+    theta_value = sess.run(theta, feed_dict={X:x, Y:y})
+    print (theta_value)
 
-# plotting
-XX, YY = Xdata.T, Ydata.T
-plt.plot(XX, YY, 'bo', label="data")
-plt.plot(XX, XX*wb+bb, 'r', label='predicted')
+# plotting the data
+plt.plot(x[:, 1], y, 'bo', label='data')
+plt.plot(x[:, 1], x[:, 1]*theta_value[1, 0] + theta_value[0, 0], 'r', label='predicted line')
 plt.legend()
 plt.show()
