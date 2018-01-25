26df789818ea5dbc45f308dca71154ffea94c442
gicheonkang <kgc519@gmail.com>
2018-01-25 12:03:22 +0900

    week8 work done

--- week8/ex7/recoverData.m
+++ week8/ex7/recoverData.m
@@ -21,6 +21,8 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
+U_REDUCE = U(:, 1:K);
+X_rec = Z * U_REDUCE';
 
 
 % =============================================================
